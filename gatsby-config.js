00143f6a75994aeac9901f775568a59738d38b98
Darin Allen <darinallen@gmail.com>
2019-01-08 22:21:57 -0500

    Remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,5 @@
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: path.join(__dirname, 'src', 'images')
-      }
-    },
-    'gatsby-plugin-sass',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp'
-  ]
+  plugins: ['gatsby-plugin-sass']
 };
