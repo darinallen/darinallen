ed45d64ecbb69bfef67e4302957eaf3717d7eebe
Darin Allen <darinallen@gmail.com>
2019-01-10 21:27:49 -0500

    Update About section styles and copy

--- src/components/Main/About/index.js
+++ src/components/Main/About/index.js
@@ -8,30 +8,30 @@ const About = () => (
     <div className="u-center-text u-margin-bottom-large">
       <h2 className="heading-secondary">About Me</h2>
     </div>
-    <div className="row">
-      <div className="col-1-of-2">
+    <div className={styles.row}>
+      <div className={styles.circleImageContainer}>
+        <CircleImage />
+      </div>
+      <div className={styles.text}>
         <h3 className="heading-tertiary u-margin-bottom-small">Web developer</h3>
         <p className="paragraph">
-          I build websites using full-stack JavaScript and other modern web technologies. I specialize in React and
-          Node.js.
+          I build websites using full-stack JavaScript, CSS, and other modern web technologies. I specialize in React
+          and Node.js.
         </p>
 
-        <h3 className="heading-tertiary u-margin-bottom-small">Digital marketing expert</h3>
+        <h3 className="heading-tertiary u-margin-bottom-small">Digital marketer</h3>
         <p className="paragraph">
-          I understand how digital marketing and web technology are interconnected, and I would love to help you develop
-          a comprehensive web strategy that includes both aspects to achieve your business goals.
+          I understand how digital marketing and web technology are connected, and I would love to help you develop a
+          comprehensive web strategy that includes both aspects to achieve your business goals.
         </p>
 
         <h3 className="heading-tertiary u-margin-bottom-small">Mentor and educator</h3>
         <p className="paragraph">
-          I have taught software training seminars across the United States and Canada, and I love helping people and
+          I've taught software training seminars across the United States and Canada, and I love helping people and
           teams grow and develop their skills.
         </p>
 
         <ButtonText link="#features" text="Learn more &rarr;" />
-      </div>
-      <div className="col-1-of-2">
-        <CircleImage />
       </div>
     </div>
   </section>
