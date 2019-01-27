fd89e26a961ffdc12e25375983664690308d6654
Darin Allen <darinallen@gmail.com>
2019-01-26 21:05:35 -0500

    Center About content

--- src/components/Main/About/index.js
+++ src/components/Main/About/index.js
@@ -8,41 +8,43 @@ const About = ({ updateBeverage }) => (
     <div className="u-center-text u-margin-bottom-large">
       <h2 className="heading-secondary">About Me</h2>
     </div>
-    <div className={styles.row}>
-      <div className={styles.circleImageContainer}>
-        <CircleImage />
-      </div>
-      <div className={styles.text}>
-        <h3 className="heading-tertiary u-margin-bottom-small">Web developer</h3>
-        <p className="paragraph">
-          I build websites using full-stack JavaScript, CSS, and other modern web technologies. Every site I build is
-          designed from the ground up to help you achieve your online business goals.
-        </p>
+    <div className={styles.content}>
+      <div className={styles.row}>
+        <div className={styles.circleImageContainer}>
+          <CircleImage />
+        </div>
+        <div className={styles.text}>
+          <h3 className="heading-tertiary u-margin-bottom-small">Web developer</h3>
+          <p className="paragraph">
+            I build websites using full-stack JavaScript, CSS, and other modern web technologies. Every site I build is
+            designed from the ground up to help you achieve your online business goals.
+          </p>
 
-        <h3 className="heading-tertiary u-margin-bottom-small">Digital marketer</h3>
-        <p className="paragraph">
-          I understand how digital marketing and web technology are connected, and I want to help you develop a
-          comprehensive web strategy that includes both.
-        </p>
+          <h3 className="heading-tertiary u-margin-bottom-small">Digital marketer</h3>
+          <p className="paragraph">
+            I understand how digital marketing and web technology are connected, and I want to help you develop a
+            comprehensive web strategy that includes both.
+          </p>
 
-        <h3 className="heading-tertiary u-margin-bottom-small">Mentor and educator</h3>
-        <p className="paragraph">
-          I love working with people, and I genuinely enjoy teaching individuals and teams how to leverage websites to
-          build strong businesses. Let's grab <span className={styles.coffee}>coffee</span>,{' '}
-          <span className={styles.tea}>tea</span>, or <span className={styles.beer}>beer</span>, and start the
-          conversation.
-        </p>
+          <h3 className="heading-tertiary u-margin-bottom-small">Mentor and educator</h3>
+          <p className="paragraph">
+            I love working with people, and I genuinely enjoy teaching individuals and teams how to leverage websites to
+            build strong businesses. Let's grab <span className={styles.coffee}>coffee</span>,{' '}
+            <span className={styles.tea}>tea</span>, or <span className={styles.beer}>beer</span>, and start the
+            conversation.
+          </p>
 
-        <div className={styles.btnContainer}>
-          <ButtonText link="#contact" onClick={() => updateBeverage('coffee')} primary>
-            Mmmm, coffee ☕
-          </ButtonText>
-          <ButtonText link="#contact" onClick={() => updateBeverage('tea')} tertiary>
-            Mmmm, tea 🍵
-          </ButtonText>
-          <ButtonText link="#contact" onClick={() => updateBeverage('beer')} secondary>
-            Mmmm, beer 🍺
-          </ButtonText>
+          <div className={styles.btnContainer}>
+            <ButtonText link="#contact" onClick={() => updateBeverage('coffee')} primary>
+              Mmmm, coffee ☕
+            </ButtonText>
+            <ButtonText link="#contact" onClick={() => updateBeverage('tea')} tertiary>
+              Mmmm, tea 🍵
+            </ButtonText>
+            <ButtonText link="#contact" onClick={() => updateBeverage('beer')} secondary>
+              Mmmm, beer 🍺
+            </ButtonText>
+          </div>
         </div>
       </div>
     </div>
