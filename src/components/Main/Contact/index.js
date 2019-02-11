0dbda7c22d646b436218ff77f655ed76b053b16f
Darin Allen <darinallen@gmail.com>
2019-02-10 22:22:43 -0500

    Testing env variables

--- src/components/Main/Contact/index.js
+++ src/components/Main/Contact/index.js
@@ -20,6 +20,7 @@ class Contact extends React.Component {
   };
 
   sendEmail = () => {
+    console.log('process.env.EMAILJS_USER_ID: ', process.env.EMAILJS_USER_ID);
     const { name, email, phone, concept } = this.state;
     const { beverage } = this.props;
     let contactNumber = ((Math.random() * 100000) | 0).toString();
@@ -35,7 +36,8 @@ class Contact extends React.Component {
 
     const serviceId = 'default_service';
     const templateId = 'contact_form';
-    emailjs.send(serviceId, templateId, templateParams, process.env.EMAILJS_USER_ID);
+    console.log({ name, email, phone, concept });
+    // emailjs.send(serviceId, templateId, templateParams, process.env.EMAILJS_USER_ID);
   };
 
   handleSubmit = e => {
