12811f448fcefab8ae9ac3dc00166091ff23f139
Darin Allen <darinallen@gmail.com>
2019-02-15 15:01:41 -0500

    Update Google Analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,18 +5,9 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-134319049-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10
+        trackingId: 'UA-134319049-1'
       }
     }
   ]
